a89b9b99b6d09c0fa259d4b8312fcba29c4d01c8
farukilic <farukkilicaga@gmail.com>
2024-04-30 01:36:40 +0300

    Digital Agenda

--- Dijital Ajanda/Dijital Ajanda.cpp
+++ Dijital Ajanda/Dijital Ajanda.cpp
@@ -6,7 +6,9 @@
 #include <stdio.h>
 #include <string>
 #include <ctime>
-
+#include <fstream>
+#include <vector>
+using namespace std;
 /*
 GEREKSİNİMLER:
 -Kullanıcının girebileceği bilgiler: Görevler, notlar
@@ -14,34 +16,66 @@ GEREKSİNİMLER:
 -Hatırlatıcı olacak
 -Veriler dosyada tutulacak
 
-
-
-
 */
-int main() {
-	setlocale(LC_ALL,"Turkish");
-	using namespace std;
-
-	string baslik;
-	string notlar;
-	string aciklama;
-
-	//Tarih tutma
-	//Tarih tutarken çıkan hatayı engelliyor. Güvenlik açığı veriyor.
-	time_t simdiki_zaman = time(nullptr);
-	tm* tarih = localtime(&simdiki_zaman);
-	cout << "Şu an: "; cout << asctime(tarih) << endl;
-
-	//Giriş kısmında alınacak bilgiler(Kullanıcı arayüzü)
+struct User {
 	string kullanici_adi;
 	string isim;
 	string soyisim;
 	string sifre;
 	int yas;
+};
 
-	
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	User user;
 
+	//Tarih tutma
+	time_t simdiki_zaman = time(nullptr);
+	tm* tarih = localtime(&simdiki_zaman);
+	cout << "Şu an: "; cout << asctime(tarih) << endl;
 
+	//Giriş kısmında alınacak bilgiler(Kullanıcı arayüzü)
 
+	cout << "8 ile 12 hane arasında bir kullanıcı adı belirleyiniz: \n";
+	getline(cin, user.kullanici_adi);
+	while (user.kullanici_adi.size() < 8 || user.kullanici_adi.size() > 12) {
+		cout << "8 ile 12 hane arasında bir kullanıcı adı belirleyiniz: \n";
+		getline(cin, user.kullanici_adi);
+	}
+
+	cout << "İsminizi giriniz: \n";
+	getline(cin, user.isim);
+	while (user.isim.size() < 2 || user.isim.size() > 14) {
+		cout << "8 ile 12 hane arasında bir isim giriniz: \n";
+		getline(cin, user.isim);
+	}
+
+	cout << "Soyisminizi giriniz: \n";
+	getline(cin, user.soyisim);
+	while (user.soyisim.size() < 1 || user.soyisim.size() > 14) {
+		cout << "8 ile 12 hane arasında bir soyisim giriniz: \n";
+		getline(cin, user.soyisim);
+	}
+
+	cout << "Şifre belirleyiniz: (8 ile 18 karakter arasında olmalı) \n";
+	getline(cin, user.sifre);
+	while (user.sifre.size() < 8 || user.sifre.size() > 18) {
+		cout << "Şifre belirleyiniz : (8 ile 18 karakter arasında olmalı)  \n";
+		getline(cin, user.sifre);
+	}
+
+	cout << "Yaşınız kaç: \n";
+	cin >> user.yas;
+	while ((13 < user.yas) && (user.yas > 120)) {
+		cout << "Yaşınız uygun değildir! \n";
+		cin >> user.yas;
+	}
+	ofstream dosya("Veri.txt");
+	dosya << user.kullanici_adi;
+	dosya << user.isim;
+	dosya << user.soyisim;
+	dosya << user.yas;
+	dosya << user.sifre;
+	dosya.close();
 
 }
