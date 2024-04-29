ae47842ec337145fa3911e119434fee9a2db83b9
farukilic <farukkilicaga@gmail.com>
2024-04-29 18:20:36 +0300

    Digital Agenda
    C++ ile geliştirildi. Dijital ajanda uygulamasıdır.

---
+++ Dijital Ajanda/Dijital Ajanda.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <list>
+#include <random>
+#include <stdio.h>
+#include <string>
+
+/*
+GEREKSİNİMLER:
+-Kullanıcının girebileceği bilgiler: Görevler, notlar
+-Arama ve filtreleme olmayacak
+-Hatırlatıcı olacak
+-Veriler dosyada tutulacak
+
+
+
+
+*/
+int main() {
+	setlocale(LC_ALL,"Turkish");
+	using namespace std;
+
+	string baslik;
+	string notlar;
+	string aciklama;
+	int tarih;
+
+	//Dosyayı okutma ve üstüne yazma kısmı
+	ofstream 
+
+
+
+
+
+}
